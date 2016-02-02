f7be300c5cca28d62bcf415d85792496ee80de55
Brandon Carl <brandon.j.carl@gmail.com>
2016-02-02 13:14:07 -0500

    Extensive commenting

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
+/**
 
-/*
+  motors
+  Copyright 2016 Brandon Carl
+  MIT Licensed
 
-  Motors
+  Class
+  • compile
+  • compileFile
+  • addEngine
+  • createEngine
+  • hasEngine
+  • removeEngine
 
-*/
+**/
 
 "use strict";
 
 
-/*
+//
+//  Dependencies
+//
 
-  //// Dependencies
-
-*/
-
-var fs        = require("fs"),
-    path      = require("path"),
-    preschool = require("preschool"),
-    core,
+var fs           = require("fs"),
+    path         = require("path"),
+    preschool    = require("preschool"),
     Motors;
 
 
 
-/*
 
-  //// Defaults
+//
+//  Core mappings
+//
 
-*/
-
-core = {
+var core = {
   "js"   : "javascript",
   "html" : "html",
   "css"  : "css"
@@ -36,18 +41,26 @@ core = {
 
 
 
-/*
+/**
 
-  //// Class definition
+  Constructor for Motors class. Automatically installs packages by default.
 
-*/
+  @param {Object} mappings Keys correspond to extensions, values to engine names.
+  @param {Object} options Install directory `dir`, and whether to `fetch` if missing.
+  @returns {Motors} Instance of class.
 
+  @example
+  var motors = new Motors({ dir : process.cwd() });
+
+**/
 
 module.exports = Motors = function(options) {
 
+  // Set up defaults
   options = Object.assign({ fetch : true }, options);
   this.mappings = Object.assign(core, options.mappings);
 
+  // Initialize engines
   this.engines = {};
   for (var ext in this.mappings)
     this.addEngine(ext, this.mappings[ext]);
@@ -62,6 +75,22 @@ module.exports = Motors = function(options) {
 
 
 
+/**
+
+  compiles a string using engine associated with ext
+
+  @param {String} ext The extension associated with string (e.g. "coffee").
+  @param {String} str The string to be compiled.
+  @param {Object} [options={}] Options to be passed to rendering engine.
+  @param {Function} next Callback of type fn(err, compiled).
+
+  @example
+  motors.compile("coffee", "console.log 'Hello'", function(err, compiled) {
+    console.log(compiled);
+    // => console.log('Hello');
+  });
+
+**/
 
 Motors.prototype.compile = function(ext, str, options, next) {
 
@@ -70,6 +99,21 @@ Motors.prototype.compile = function(ext, str, options, next) {
 };
 
 
+
+/**
+
+  compiles a file using engine associated with file's extension
+
+  @param {String} filename Name of file.
+  @param {Object} [options={}] Options to be passed to rendering engine.
+  @param {Function} next Callback of type fn(err, compiled).
+
+  @example
+  motors.compileFile("app.coffee", function(err, compiled) {
+    // Compiled version of app.coffee
+  });
+
+**/
 
 Motors.prototype.compileFile = function(filename, options, next) {
 
@@ -84,12 +128,7 @@ Motors.prototype.compileFile = function(filename, options, next) {
 };
 
 
-/*
-
-   Helper function for async iteration of engines
-
-*/
-
+// Helper function for async iteration of engines
 function runTask(tasks, str, options, next) {
 
   var task = tasks.shift();
@@ -102,6 +141,20 @@ function runTask(tasks, str, options, next) {
 
 }
 
+
+/**
+
+  Returns a compilation function based on input chain.
+
+  @param {String} chain String containing names of engines to use.
+  @returns {Function} Compilation function fn(str, options, next)
+
+  @example
+  motors.createEngine("jade");
+  motors.createEngine("jade>handlebars");
+  motors.createEngine("js>uglify-js");
+
+*/
 
 Motors.prototype.createEngine = function(chain) {
 
@@ -120,6 +173,38 @@ Motors.prototype.createEngine = function(chain) {
 };
 
 
+
+/**
+
+  Gets an engine based on chain, creating engine if necessary.
+
+  @param {String} chain Chain of engines.
+
+  @example
+  motors.getEngine("jade>handlebars");
+
+**/
+
+Motors.prototype.getEngine = function(chain) {
+
+  if (!this.engines[chain])
+    this.engines[chain] = this.createEngine(chain);
+
+  return this.engines[chain];
+
+};
+
+
+
+/**
+
+  Adds an engine for an extension, creating engine if necessary.
+
+  @param {String} ext File extension to be associated with engine.
+  @param {String} [chain] Optional chain for creating engine.
+
+**/
+
 Motors.prototype.addEngine = function(ext, chain) {
 
   chain = chain || preschool.defaultEngineForExtension(ext);
@@ -133,6 +218,15 @@ Motors.prototype.addEngine = function(ext, chain) {
 };
 
 
+
+/**
+
+  Removes an engine by extension.
+
+  @param {String} ext File extension to for removal.
+
+**/
+
 Motors.prototype.removeEngine = function(ext) {
 
   // Delete both from mappings and engine
@@ -144,6 +238,16 @@ Motors.prototype.removeEngine = function(ext) {
 
 };
 
+
+
+/**
+
+  Determines if an engine currently exists for an extension.
+
+  @param {String} ext File extension to check.
+  @returns {Boolean} Whether engine exists.
+
+**/
 
 Motors.prototype.hasEngine = function(ext) {
 
